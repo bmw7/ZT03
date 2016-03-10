11d6fd2a44133a3f7abdad26d1de3bcb90f37ebb
zhangtao <a@mavict.com>
2016-03-11 07:54:36 +0800

    add login

--- pcms/Application/Admin/Controller/LoginController.class.php
+++ pcms/Application/Admin/Controller/LoginController.class.php
@@ -1,5 +1,6 @@
 <?php
 namespace Admin\Controller;
+use Think\Controller;
 
 class LoginController extends Controller{
     public function index(){
@@ -11,15 +12,34 @@ class LoginController extends Controller{
     	$username = I('post.username');
     	$password = I('post.password');
     	
-    	$Account = M('Account');
-    	$password_db = $Account->where('username = '.$username)->getField('password');
+     	$Account = M('Account');
+     	$att = $Account->where("username = '%s'",$username)->select();
     	
-    	$CryptService = A('Crypt','Service');
-    	$password_cr = $CryptService->work($password);
-    	
-    	if ($password_db == $password_cr){
+    	if (count($att) == 1){
+    		$password_db = $att[0][password]; // 数据库中存储密码
+    		$CryptService = A('Crypt','Service');
+    		$password_cr = $CryptService->work($password); // 前台传递密码加密
     		
+    		// 密码校验通过
+    		if ($password_db == $password_cr){
+    			
+    			$info = array();
+    			array_push($info, $username);
+    			array_push($info, $att[0]['login_count']+1);
+    			array_push($info, $att[0]['login_date']);
+    			array_push($info, $att[0]['login_address']);
+    			session('login',$info);
+    			
+    			$att[0]['login_address'] = I('post.address');
+    			$att[0]['login_count'] += 1;
+    			$att[0]['login_date'] = date('Y-m-d H:i:s');
+    			  			
+    			$Account->save($att[0]);
+    			$this->ajaxReturn("");
+    		}
     	}
+
+    	$this->ajaxReturn("failed");
     }
 
     
