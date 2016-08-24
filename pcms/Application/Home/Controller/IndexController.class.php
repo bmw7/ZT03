d5b62877421b36532f7f28649ac8903d031d0521
zhangtao <z@mavict.com>
2016-08-24 16:54:29 +0800

    add install_database() function

--- pcms/Application/Home/Controller/IndexController.class.php
+++ pcms/Application/Home/Controller/IndexController.class.php
@@ -10,4 +10,31 @@ class IndexController extends Controller {
     	$this->display();
     }
     
+    // 安装数据库
+    public function install_database(){
+    	$link = mysql_connect(C('DB_HOST'), C('DB_USER'), C('DB_PWD'));
+    	$result = mysql_select_db(C('DB_NAME'), $link); // 若存在，则值为 1
+    
+    	// 数据库不存在，创建
+    	if ($result != 1){
+    		// 该句必加。否则乱码
+    		mysql_query("SET NAMES utf8");
+    
+    		mysql_query("CREATE DATABASE ".C('DB_NAME'));
+    		mysql_query("USE ".C('DB_NAME'));
+    
+    		$_sql = file_get_contents('database.sql');
+    		$_arr = explode(';', $_sql);
+    
+    		//执行sql语句
+    		foreach ($_arr as $_value) {
+    			mysql_query($_value.';');
+    			echo explode('(', $_value)[0].'  安装完毕<br>';
+    		}
+    
+    	}else{
+    		echo '<h1>数据库已经存在。本操作系统已经记录，请勿再尝试。</h1>';
+    	}
+    }
+    
 }
