398dacb7db51d4882a22024c03da48ec76ba6585
zhangtao <z@mavict.com>
2016-09-04 17:10:58 +0800

    modify home

--- pcms/Public/client/js/left_menu.js
+++ pcms/Public/client/js/left_menu.js
@@ -8,15 +8,22 @@ $(document).ready(function() {
 		
 		// 子菜单是关闭状态 url指的是当前页面的路径
 		if ($(this).next().css('display') == 'none') {
-			$(this).find('a').css('background-image', 'url(PUBLIC/client/img/left_menu.jpg)');
+			$(this).find('a').css('background-image', 'url(Public/client/img/left_menu.jpg)');
 		// 子菜单是开启状态
 		}else{
-			$(this).find('a').css({'background-image':'url(PUBLIC/client/img/left_menu_out.jpg)','font-weight':'bold'});
-			$('.menu_left_item').not(this).find('a').css({'background-image':'url(PUBLIC/client/img/left_menu.jpg)','font-weight':'normal'});
+			$(this).find('a').css({'background-image':'url(Public/client/img/left_menu_out.jpg)','font-weight':'bold'});
+			$('.menu_left_item').not(this).find('a').css({'background-image':'url(Public/client/img/left_menu.jpg)','font-weight':'normal'});
 			$('.menu_left_item').not(this).next().each(function(){
 				if ($(this).attr('class') == 'menu_left_item_sub') { $(this).hide(); };
 			});
 		};	
+	});
+
+
+
+	$(document).on("click",".sub_item",function(){
+			$(this).find('a').css({'background-image':'url(Public/client/img/left_menu_in.jpg)','font-weight':'bold'});
+			$('.sub_item').not(this).find('a').css({'background-image':'url(Public/client/img/left_menu.jpg)','font-weight':'normal'});
 	});
 	
 	
