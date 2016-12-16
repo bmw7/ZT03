933844c6c5427cc4409f79c63f692ab40bd26154
zhangtao <z@mavict.com>
2016-12-16 18:50:15 +0800

    first commit

--- pcms/Application/Common/Conf/config.php
+++ pcms/Application/Common/Conf/config.php
@@ -2,9 +2,9 @@
 return array(
 	'DB_TYPE'   => 'mysql',     // 数据库类型
     'DB_HOST'   => 'localhost', // 服务器地址
-    'DB_NAME'   => 'wenzhou',       // 数据库名
-    'DB_USER'   => 'root',      // 用户名
-    'DB_PWD'    => 'zhangtao',  // 密码
+    'DB_NAME'   => 'zhangtao_wenzhou',       // 数据库名
+    'DB_USER'   => 'zhangtao_wenzhou',      // 用户名
+    'DB_PWD'    => 'Zhangtao2016',  // 密码
     'DB_PORT'   => 3306,        // 端口
     'DB_PREFIX' => 'tp_',       // 数据库表前缀 
     'DB_CHARSET'=> 'utf8'       // 字符集
--- pcms/Application/Home/Controller/IndexController.class.php
+++ pcms/Application/Home/Controller/IndexController.class.php
@@ -11,16 +11,17 @@ class IndexController extends Controller {
     }
     
     // 安装数据库
-    public function install_database(){
+    /*
+    public function install(){
     	$link = mysql_connect(C('DB_HOST'), C('DB_USER'), C('DB_PWD'));
     	$result = mysql_select_db(C('DB_NAME'), $link); // 若存在，则值为 1
     
     	// 数据库不存在，创建
-    	if ($result != 1){
+    	if ($result == 1){
     		// 该句必加。否则乱码
     		mysql_query("SET NAMES utf8");
     
-    		mysql_query("CREATE DATABASE ".C('DB_NAME'));
+    		//mysql_query("CREATE DATABASE ".C('DB_NAME'));
     		mysql_query("USE ".C('DB_NAME'));
     
     		$_sql = file_get_contents('database.sql');
@@ -33,8 +34,54 @@ class IndexController extends Controller {
     		}
     
     	}else{
-    		echo '<h1>数据库已经存在。本操作系统已经记录，请勿再尝试。</h1>';
+    		echo '<h1>没有安装 </h1>';
     	}
-    }
+    }*/
+    
+    
+    // 数据恢复
+    /*
+    public function backup(){
+    	$start = I('get.start');
+    	$cid = I('get.cid');
+    	 
+    	 
+    	$Article = M('Article');
+    
+    	$link = mysql_connect('rdss826cy1y4at8og84ro.mysql.rds.aliyuncs.com', 'zhangtao', 'Zhangtao2015');
+    	$result = mysql_select_db('wenzhou', $link); // 若存在，则值为 1
+    	mysql_query("SET NAMES utf8");
+    
+    	$count = mysql_fetch_array(mysql_query("select count(*) from article where articleCategoryId = ".$cid));
+    	 
+    	$result = mysql_query("select * from article where articleCategoryId = ".$cid." order by id asc limit ".$start.",50");
+    
+    	while($rs = mysql_fetch_array($result)){
+    		$data['title'] = $rs['title'];
+    		$data['create_date'] = $rs['createDate'];
+    		$data['content'] = $rs['content'];
+    		$data['category_id'] = $cid;
+    		$data['category_url'] = 'docs';
+    		$data['seo_keywords'] = $rs['seoKeywords'];
+    		$data['seo_description'] = $rs['seoDescription'];
+    		$Article->add($data);
+    	}
+    	 
+    	$this->assign('count',$count[0]);
+    	$this->assign('end',$start+50);
+    	$this->display();
+    	 
+    }*/
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
