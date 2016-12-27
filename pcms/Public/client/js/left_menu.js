f79e03cc3cd05f61a2cfdbc3f046b8ee827cf434
zhangtao <z@mavict.com>
2016-12-27 20:32:20 +0800

    modify

--- pcms/Public/client/js/left_menu.js
+++ pcms/Public/client/js/left_menu.js
@@ -6,17 +6,45 @@ $(document).ready(function() {
 			$(this).next().toggle();
 		};
 		
-		// 子菜单是关闭状态 url指的是当前页面的路径
+		// 子菜单是关闭状态     url指的是当前页面的路径
 		if ($(this).next().css('display') == 'none') {
-			$(this).find('a').css('background-image', 'url(Public/client/img/left_menu.jpg)');
+			$(this).find('a').css({'background-image':'url(Public/client/img/left_menu.jpg)','font-weight':'normal'});	
+			
+			// 鼠标离开
+			$(this).find('a').mouseout(function(){
+				$(this).css({'background-image':'url(Public/client/img/left_menu.jpg)','font-weight':'normal'});
+			});
+			
 		// 子菜单是开启状态
 		}else{
-			$(this).find('a').css({'background-image':'url(Public/client/img/left_menu_out.jpg)','font-weight':'bold'});
 			$('.menu_left_item').not(this).find('a').css({'background-image':'url(Public/client/img/left_menu.jpg)','font-weight':'normal'});
 			$('.menu_left_item').not(this).next().each(function(){
 				if ($(this).attr('class') == 'menu_left_item_sub') { $(this).hide(); };
 			});
+			
+			
+			// 给其他项目添加鼠标滑过效果
+			$('.menu_left_item').not(this).find('a').hover(
+					function(){
+						$(this).css({'background-image':'url(Public/client/img/left_menu_out.jpg)','font-weight':'bold'});
+					},
+					function(){
+						$(this).css({'background-image':'url(Public/client/img/left_menu.jpg)','font-weight':'normal'});
+					}
+			);
+			
+			$(this).find('a').hover(
+					function(){
+						$(this).css({'background-image':'url(Public/client/img/left_menu_out.jpg)','font-weight':'bold'});
+					},
+					function(){
+						$(this).css({'background-image':'url(Public/client/img/left_menu_out.jpg)','font-weight':'bold'});
+					}
+			);
+			
 		};	
+		
+		
 	});
 
 
@@ -24,6 +52,28 @@ $(document).ready(function() {
 	$(document).on("click",".sub_item",function(){
 			$(this).find('a').css({'background-image':'url(Public/client/img/left_menu_in.jpg)','font-weight':'bold'});
 			$('.sub_item').not(this).find('a').css({'background-image':'url(Public/client/img/left_menu.jpg)','font-weight':'normal'});
+			
+			// 给其他项目添加鼠标滑过效果
+			$('.sub_item').not(this).find('a').hover(
+					function(){
+						$(this).css({'background-image':'url(Public/client/img/left_menu_in.jpg)','font-weight':'bold'});
+					},
+					function(){
+						$(this).css({'background-image':'url(Public/client/img/left_menu.jpg)','font-weight':'normal'});
+					}
+			);
+			
+			$(this).find('a').hover(
+					function(){
+						$(this).css({'background-image':'url(Public/client/img/left_menu_in.jpg)','font-weight':'bold'});
+					},
+					function(){
+						$(this).css({'background-image':'url(Public/client/img/left_menu_in.jpg)','font-weight':'bold'});
+					}
+			);
+			
+			
+			
 	});
 	
 	
