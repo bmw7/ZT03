02f3e97eb8921c41a113b507bab10f80229caf8c
zhangtao <a@biushop.com>
2018-01-10 15:57:36 +0800

    begining

--- pcms/Application/Common/Conf/config.php
+++ pcms/Application/Common/Conf/config.php
@@ -2,9 +2,9 @@
 return array(
 	'DB_TYPE'   => 'mysql',     // 数据库类型
     'DB_HOST'   => 'localhost', // 服务器地址
-    'DB_NAME'   => 'cms',       // 数据库名
+    'DB_NAME'   => 'biushop',       // 数据库名
     'DB_USER'   => 'root',      // 用户名
-    'DB_PWD'    => 'zhangtao',  // 密码
+    'DB_PWD'    => 'Zhangtao2017',  // 密码
     'DB_PORT'   => 3306,        // 端口
     'DB_PREFIX' => 'tp_',       // 数据库表前缀 
     'DB_CHARSET'=> 'utf8'       // 字符集
